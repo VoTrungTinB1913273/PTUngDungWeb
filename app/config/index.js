45dea9ca2e35bfecd47bfa559517ccf599d43c22
TinVo998 <90139129+TinVo998@users.noreply.github.com>
2021-10-19 11:42:57 +0700

    thay doi all

--- app/config/index.js
+++ app/config/index.js
@@ -1,13 +1,11 @@
 const config = {
-    app :{
-        port: process.env.PORT || 8080,
-        origin: [
-            "http://localhost:8081"
-        ]
+    app:{
+        port : process.env.PORT||8080,
+       
     },
     db: {
         url: "mongodb://localhost:27017/contactbook"
-    } 
+        }
 };
 
 module.exports = config;
--- server.js
+++ server.js
@@ -1,44 +1,52 @@
 const express = require("express");
 const cors = require("cors");
 const config = require("./app/config");
-const app = express();
-const{BadRequestError} = require("./app/helpers/errors");
 const setupContactRoutes = require("./app/routes/contact.routes");
-app.use(cors({origin: config.app.origins}));
+const { BadRequestError } = require("./app/helpers/errors");
+const db = require("./app/models");
+const app = express();
 
-setupContactRoutes(app);
+var corOptions = {
+ origin: "http://localhost:8081"
+};
+app.use(cors(corOptions));
+
 app.use(express.json());
 
-app.use(express.urlencoded({extended: true}));
-//handler 404
-app.use((req , res,next)=>{
-    next(new BadRequestError(404 ,"Resourse not found"));
-});
-//mongoose
-app.use(express.urlencoded({extended:true}));
-const db = require("./app/models");
-db.mongoose.connect(config.db.url)
-    .then(()=>{
-        console.log("Connected to the database!");
-    })
-    .catch((error)=>{
-        console.log("Cannot connect to the database!", error);
-        process.exit();
-    });
+app.use(express.urlencoded({ extended: true }));
 
-    
-app.use((err,req,res,next)=>{
-    console.log(err);
-    res.status(err.statusCode  || 500).json({
-        message: err.message || "Internal Server Error",
-    });
+db.mongoose
+  .connect(config.db.url)
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((error) => {
+    console.log("Cannot connect to the database!", error);
+    process.exit();
+  });
+
+
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome contact book application." });
+});
+setupContactRoutes(app);
+
+app.use((req, res, next) => {
+  next(new BadRequestError(404, "Resource not found"));
 });
 
-app.get("/",(req, res) =>{
-    res.json({message: "Welcome to contact book application"});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
 });
+
 
 const PORT = config.app.port;
-app.listen(PORT, ()=>{
-    console.log(`Server is running on post ${PORT}.`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
